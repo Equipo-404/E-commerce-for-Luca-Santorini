1ac92ecbfcaccccf097ccd60cbfdad41da696db6
Beto <josealberto.fb15@gmail.com>
2023-02-05 12:44:11 -0600

    ci acerca de

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { AcercaDe404 } from './components/acerca404/AcercaDe404';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todo me male sal</h1>
+        <AcercaDe404/>
       </header>
     </div>
   );
