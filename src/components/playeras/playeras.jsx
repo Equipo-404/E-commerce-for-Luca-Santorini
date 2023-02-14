b8e6a351ceb6c0f03c913e4ff95cde5fedf7bb42
Leonardo Miranda <leonardmiranda99@gmail.com>
2023-02-14 01:20:05 -0600

    incorporacion a react y responsibilidad lograda

--- src/components/playeras/playeras.jsx
+++ src/components/playeras/playeras.jsx
@@ -1,60 +1,196 @@
 import React from 'react'
-import { Card, CardBody, CardTitle, CardSubtitle, CardText,Button } from 'reactstrap';
+import {  CardBody, CardTitle, CardSubtitle, Button, Container,Row, Col } from 'reactstrap';
 import "./playeras.css";
 
 const Playeras = () => {
   return (
     <>
       <h1>NavBar</h1>
-      <div class="foto1">
-        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676229431/IMG_9868_vpuxgk.jpg" alt="hombre y mujer sentados en la acera con un skate al lado con payeras puestas love 4 galli blancas"></img>
+      <div className='foto1'>
+       
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676229431/IMG_9868_vpuxgk.jpg" alt="hombre y mujer sentados en la acera con un skate al lado con payeras puestas love 4 galli blancas" className='img-fluid'></img>
+     
       </div>
+      <Container >
+        <Row>
+          
       <div class="segundotexto"><p>Playeras</p></div>
 
-      <div class="fila">
      
-        <Card className='columna'>
-        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" alt="playera negra de danny ocean babylon"></img>
+      <div className='fila'>
+        <Col md={3} >
+          <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" alt="playera negra de danny ocean babylon"></img></div>
+        
           <CardBody>
-            <CardTitle tag="h5">
-              Card title
+            <CardTitle  className='titulo' tag="h5">
+              Playera
             </CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
+            <CardSubtitle className="precio" tag="h6">
+              $199.99 MXN
             </CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the bulk of the cards content.
-            </CardText>
-            <Button>
-              Button
+            <Button className='boton'>
+              Añadir a la bolsa
             </Button>
           </CardBody>
-        </Card>
+          </div>
+        </Col>
+        
+
+        <Col md={3} >
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676227552/HARRY_STYLES_LOVE_ON_TOUR_BLANCA_ESPALDA_kurmeg.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $150.00 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
+        
+        <Col md={3} >
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676228105/DADDY_YANKEE_X_LSSW_NEGRA_FRONTAL_q72mpq.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $150.00 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
+        </div>
+
+        <div className='fila'>
+        <Col md={3} >
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676228252/LOVE_4_GALLI_BLANCA_ESPALDA_dyqj2b.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $150.00 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
       
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" alt="playera negra de danny ocean babylon"></img>
-
+        <Col md={3}>
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158216/LSSW_x_DANYY_OCEAN_BABYLON_G_VERDE_ESPALDA_plybwz.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $199.00 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
+      
+        <Col md={3}>
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676229206/blnaca_basica_dmhifj.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $99.00 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
         </div>
 
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676227552/HARRY_STYLES_LOVE_ON_TOUR_BLANCA_ESPALDA_kurmeg.png" alt="playera blanca harry styles liveo on tour"></img>
+        <div className='fila'>
+        <Col md={3} >
+          <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676356841/BASIC_VERDE_X_LSSW_rtnbzi.png"></img></div>
+        
+          <CardBody>
+            <CardTitle  className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+              $99.90 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
+        
 
+        <Col md={3} >
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676356621/BASIC_GRIS_X_LSSW_szqmmc.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+            $99.90 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
+        
+        <Col md={3} >
+        <div className='columna'>
+          <div className='imagenespolaroid'>
+        <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676356438/BASIC_NEGRO_X_LSSW_w5r1wz.png" alt="playera negra de danny ocean babylon"></img></div>
+          <CardBody>
+            <CardTitle className='titulo' tag="h5">
+              Playera
+            </CardTitle>
+            <CardSubtitle className="precio" tag="h6">
+            $99.90 MXN
+            </CardSubtitle>
+            <Button className='boton'>
+              Añadir a la bolsa
+            </Button>
+          </CardBody>
+          </div>
+        </Col>
         </div>
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676228105/DADDY_YANKEE_X_LSSW_NEGRA_FRONTAL_q72mpq.png" alt="playera gra daddy yankee estampado bellakeo old school"></img>
-        </div>
-      </div>
-      <div class="fila">
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676228252/LOVE_4_GALLI_BLANCA_ESPALDA_dyqj2b.png" alt="playera love 4 galli blanca con estampado en color rojo y negro " ></img>
-        </div>
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158133/LSSW_DANNY_OCEAN_BABYLON_G_ROJA_ESPALDA_mgnxb1.png" alt="playera roja danny ocean babylon "></img>
-        </div>
-        <div class="columna">
-          <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676229206/blnaca_basica_dmhifj.png"></img>
-        </div>
-      </div>
+
+      </Row>
+      </Container>
     </>
   )
 }
