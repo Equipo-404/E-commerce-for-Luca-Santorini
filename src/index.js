e55936ba40252bb217034887281c4db1b992cab2
PavelOsw <pavel.osw117@gmail.com>
2023-02-19 15:42:43 -0600

    Eliminación de importaciones vacías

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Index }  from './components/login/LogIn';
-// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 
