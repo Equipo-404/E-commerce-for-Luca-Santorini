0ac8fbf83ec7b485098bc97f0f46e2739b604a91
Marcos Mar <marcosabel0810@gmail.com>
2023-02-18 23:41:12 -0600

    ci:pagina de inicio refactor: cambios de id pagina de inicio y navbar

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import imagenInicio from './ImagenInicio.png';
+import imagenInicioLucaSanto from './ImagenInicioLucaSanto.png';
 import './inicio.css';
 
 function Inicio() {
   return (
-    <div>
-      <img src={imagenInicio} id="foto1" />
-      <div className="segundotexto">
-        <h2>Novedades</h2>
-      </div>
-      <div className="fila">
+    <div className='SgundaSeccionInicio'>
+      <img src={imagenInicio} id="ImagenPaginaInicio" />
+        <h2 id="h2Novedades">Novedades</h2>
+      <div className="ImagenesSegundaSeccionInicio">
         <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" id="columna1" />
         <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" id="columna2" />
         <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" id="columna3" />
       </div>
-      <button id="verMas">Ver mas</button>
-    </div>
+      <div>
+      <button id="VerMasPaginaInicio">Ver mas novedades</button>
+      </div>
+      <img src={imagenInicioLucaSanto} id="ImagenInicioLucaSanto" />
+      <div>
+      <h2 id="h2LucaSantoriniSeccion2">Luca Santorini</h2>
+      </div>
+      <div id="SeccionTresPaginaInicio">
+      <img src="https://res.cloudinary.com/dzrqrtezc/image/upload/v1676158169/LSSW_x_DANNY_OCEAN_BABYLON_G_NEGRA_FRONTAL_cfbnyh.png" id="ImagenTerceraSeccionInicio" />
+      <div id="DescripcionPlayera">
+      <h3 id="DescripcionPlayeraInicio">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus natus atque nam architecto, modi quasi odit quisquam earum aliquid cum voluptate rerum saepe exercitationem. Fugit beatae alias quam itaque veritatis!
+        In voluptatem quo vitae laborum dolor modi voluptatum eum, ad deleniti ullam similique sequi magnam nisi sint tempore velit voluptas officia beatae quaerat incidunt, quas itaque. Officia reprehenderit quae aspernatur!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aut tempore dolorem quam ducimus fugiat alias nulla iste nihil assumenda! Veritatis quibusdam earum porro, voluptatem qui blanditiis non repellendus explicabo!</h3>
+      <button id="VerMasPaginaInicioPlayera">Ver mas playeras</button>
+      </div>
+      </div>
+      </div>
   );
 }
 
