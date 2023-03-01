28db097c1f6a17c7b7d016593d8d8ce9798cd6cb
Stephano <stephanogmili@gmail.com>
2023-02-28 23:37:56 -0600

    Actualizacion y alertas

--- src/components/login/LogIn.jsx
+++ src/components/login/LogIn.jsx
@@ -1,105 +1,96 @@
 import React, { Component } from 'react'
-import { Button, Container, Form, FormGroup, Input, Label, Row, Formulario} from 'reactstrap';
 import './style.css';
-import { useState } from 'react';
-import {validateLogin, validate, validarFormulario } from '../../login.js';
-export default class LogIn extends Component { 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { registerUser , validateLogin } from '../../login.js';
+
+
+export default class LogIn extends Component {
+    
     render() {
-    return (
-        <>
-        <html>
-        
-        <body className='booty'>
-        
-            
-
-            <div action="" className="formulario" id="formulario">
-
+        return (
+            <>
+                <html>
+                    <body className='booty'>
+                        <div clasName="section-phan" action="" className="formulario" id="formulario">
                             <h6 className="change"><span>Iniciar Sesíon </span><span>Registrarte</span></h6>
-                            <input className="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+                            <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
                             <label for="reg-log"></label>
                             <div className="card-3d-wrap mx-auto">
                                 <div className="card-3d-wrapper">
                                     <div className="card-front">
                                         <div className="center-wrap">
                                             <div className="section text-center">
-                                       
-                                                    
-                                                    <h1 className="titulitu">Iniciar Sesíon</h1>
-                                               <div className="formulario__grupo" id="grupo__nombre">
-                                               <div className="formulario__grupo-input">
-                                               <input type="email" class="formulario__input" name="logemail" id="correo" placeholder="Correo electrónico" autoComplete='off'></input>
-                                                    
-                                               </div>
-				                               <p className="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
-			                                   </div>
-
-                                                    <br></br>
-                                                    <div className="formulario__grupo" id="grupo__password">
+                                                <h1 className="titulitu">Iniciar Sesíon</h1>
+                                                <div className="formulario__grupo" id="grupo__usuario">
                                                     <div className="formulario__grupo-input">
-                                                    <input type="password" className="formulario__input" name="logpass" id="password" placeholder='Contraseña' autoComplete='off'></input>
+                                                        <input type="email" className="formulario__input" name="correo" id="correo" placeholder="Correo electrónico"></input>
+                                                        <i className="formulario__validacion-estado fas"></i>
                                                     </div>
-				                                    <p class="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
-			                                        </div>
-                                                    <br></br>
-                                                    <div>
-                                                    <button onClick={validateLogin} className="loga" type="submit">Iniciar Sesíon</button>
-                                                    </div>
-                                                    <br></br>
-                                                    <div>
+                                                    <p className={"formulario__input-error"}>El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+                                                </div>
 
-                                                <a className="parra" href="">¿Olvidaste tu contraseña?</a>
+                                                <br></br>
+                                                <div className="formulario__grupo" id="grupo__password">
+                                                    <div className="formulario__grupo-input">
+                                                        <input type="password" className="formulario__input" name="password" id="password" placeholder='Contraseña'></input>
                                                     </div>
+                                                    <p className="formulario__input-error">La contraseña tiene que tener entre 6 y 16 caracteres.</p>
+                                                </div>
+                                                <br></br>
+                                                <div>
+                                                    <button onClick={ validateLogin } className="loga" type="submit">Iniciar Sesíon</button>
+                                                </div>
+                                                <br></br>
+                                                <div>
+                                                    <a className="parra" href="">¿Olvidaste tu contraseña?</a>
                                                 </div>
                                             </div>
                                         </div>
-                                    
+                                    </div>
+
                                     <div className="card-back">
+                                        
                                         <div className="center-wrap">
                                             <div className="section text-center">
                                                 <h1 className="titulitu">Regístrate</h1>
-                                                <div class="formulario__grupo" id="grupo__nombre">
-                                                <div class="formulario__grupo-input">
-                                                <input type="text" class="formulario__input" name="regname" id="regname" placeholder="Nombre completo" autoComplete='off'></input>
-                                                    
-                                                </div>
-				                                <p class="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
-			                                    </div>
-                                                    
-                                               <div class="formulario__grupo" id="grupo__correo">
-                                               <div class="formulario__grupo-input">
-                                               <input type="email" class="formulario__input" name="logemail" id="regemail" placeholder="Correo electrónico" autoComplete='off'></input>
-                                                    
-                                               </div>
-				                               <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
-			                                   </div>
-                                               <div class="formulario__grupo" id="grupo__password">
-                                                    <div class="formulario__grupo-input">
-                                                    <input type="password" class="formulario__input" name="logpass" id="regpass"  placeholder='Contraseña' autoComplete='off'></input>
+                                                <div className="formulario__grupo" id="grupo__registro-nombre">
+                                                    <div className="formulario__grupo-input">
+                                                        <input type="text" className="formulario__input" name="nombre" id="regname" placeholder="Nombre completo"></input>
+                                                        <i class="formulario__validacion-estado fas"></i>
                                                     </div>
-				                                <p class="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
-			                                    </div>
-                                                <div class="formulario__mensaje" id="formulario__mensaje">
-				                                <p> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
-			                                    </div>
-                
-                  <br></br>                     <button onClick={validate} id="register_button" className="loga" type="submit">Registrarme</button>
-                                            <p className="formulario__mensaje-exito" id="formulario__exito"> Registro exitoso! </p>
+                                                    <p className="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos </p>
+                                                </div>
+
+                                                <div className="formulario__grupo" id="grupo__registro-usuario">
+                                                    <div className="formulario__grupo-input">
+                                                        <input type="email" className="formulario__input" name="correo" id="regemail" placeholder="Correo electrónico"></input>
+                                                        <i class="formulario__validacion-estado fas"></i>
+                                                    </div>
+                                                    <p className="formulario__input-error">El correo debe tener una estructura valida.</p>
+                                                </div>
+                                                <div className="formulario__grupo" id="grupo__registro-password">
+                                                    <div className="formulario__grupo-input">
+                                                        <input type="password" className="formulario__input" name="password" id="regpass" placeholder='Contraseña'></input>
+                                                        <i class="formulario__validacion-estado fas"></i>
+                                                    </div>
+                                                    <p className="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
+                                                </div>
+                                                <br></br>                     
+                                                <button onClick={registerUser} id="register_button" className="loga" type="submit">Registrarme</button>
+                                                <p className="formulario__mensaje-exito" id="formulario__exito"> Registro exitoso! </p>
                                             </div>
-                                            
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
-            </div>
-        
-            
-         </body>
-        </html>
-    
-    
-     </>
-    )
-}
+                        </div>
+                    </body>
+                </html>
+
+
+            </>
+        )
+    }
 
 }
--- src/login.js
+++ src/login.js
@@ -1,81 +1,123 @@
-export function validate() {
-  // Define la entrada de valores
-  var nameRegex = /^[a-zA-Z ]{2,30}$/;
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
-  
-  // Define mensaje de error
-  let nameError = 'Ingresa un nombre valido.';
-  let emailError = 'Introduce una direccion de correo valido.';
-  let passwordError = 'La contraseña debe tener entre 8-20 caractes entre ellas debe haber al menos un numero y una letra mayuscula y minuscula .';
-  
-  
-  var regnameValue = document.getElementById('regname').value;
-  var regemailValue = document.getElementById('regemail').value;
-  var regpassValue = document.getElementById('regpass').value;
-  
-  // Valida el nombre
-  if (!nameRegex.test(regnameValue)) {
-    alert(nameError);
-    return false;
+// Function to validate the registration form using regex when keyup (name, username, email, password)
+export function registerUser() {
+  // If all the fields are valid, save the user data in localStorage
+  if (validateRegisterFields()) {
+    let name = document.querySelector("#regname").value;
+    let email = document.querySelector("#regemail").value;
+    let password = document.querySelector("#regpass").value;
+    // Check if the user already exists
+    if (localStorage.getItem(email) !== null) {
+      alert("El usuario ya existe");
+      document.querySelectorAll(".formulario__grupo i").forEach((icon) => {
+        icon.classList.remove("fa-check-circle");
+      });
+      return;
+    }
+    // Save the user data in localStorage
+    let user = {
+      name: name,
+      email: email,
+      password: password
+    };
+    localStorage.setItem(email, JSON.stringify(user));
+    alert("Usuario registrado");
+    document.querySelectorAll(".formulario__grupo i").forEach((icon) => {
+      icon.classList.remove("fa-check-circle");
+    });
+    // Clear the form
+    document.querySelectorAll('input').forEach((input)=>{input.value = ''});
+    //take the user to the login page
+    document.querySelector("#reg-log").click();
+
   }
+}
+
+function validateRegisterFields() {
+  // name id = regname
+  let name = document.querySelector("#regname").value;
+  // email id = regemail
+  let email = document.querySelector("#regemail").value;
+  // password id = regpass
+  let password = document.querySelector("#regpass").value;
+
+
+  // Regex for name, email, password
+  let nameRegex = /^[a-zA-Z ]{2,30}$/;
+  let emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  let passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
   
-  // Valida correo
-  if (!emailRegex.test(regemailValue)) {
-    alert(emailError);
-    return false;
+
+  if (nameRegex.test(name)) {
+    // Name is valid
+    document.querySelector("#grupo__registro-nombre").classList.remove('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-nombre").classList.add('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-nombre i").classList.add('fa-check-circle');
+    document.querySelector("#grupo__registro-nombre i").classList.remove('fa-times-circle');
+    document.querySelector("#grupo__registro-nombre .formulario__input-error").classList.remove('formulario__input-error-activo');
+  } else {
+    // Name is invalid
+    document.querySelector("#grupo__registro-nombre").classList.add('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-nombre").classList.remove('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-nombre i").classList.add('fa-times-circle');
+    document.querySelector("#grupo__registro-nombre i").classList.remove('fa-check-circle');
+    document.querySelector("#grupo__registro-nombre .formulario__input-error").classList.add('formulario__input-error-activo');
   }
-  
-  // Valida contraseña
-  if (!passwordRegex.test(regpassValue)) {
-    alert(passwordError);
-              
-    return false;
+  if (emailRegex.test(email)) {
+    // Email is valid
+    document.querySelector("#grupo__registro-usuario").classList.remove('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-usuario").classList.add('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-usuario i").classList.add('fa-check-circle');
+    document.querySelector("#grupo__registro-usuario i").classList.remove('fa-times-circle');
+    document.querySelector("#grupo__registro-usuario .formulario__input-error").classList.remove('formulario__input-error-activo');
+  } else {
+    // Email is invalid
+    document.querySelector("#grupo__registro-usuario").classList.add('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-usuario").classList.remove('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-usuario i").classList.add('fa-times-circle');
+    document.querySelector("#grupo__registro-usuario i").classList.remove('fa-check-circle');
+    document.querySelector("#grupo__registro-usuario .formulario__input-error").classList.add('formulario__input-error-activo');
   }
-  
-  
-  // Traer valores
-  var regnameValue = document.getElementById('regname').value;
-  var regemailValue = document.getElementById('regemail').value;
-  var regpassValue = document.getElementById('regpass').value;
-  
-  // Crear objeto
-  let user = {
-    name: regnameValue,
-    email: regemailValue,
-    password: regpassValue
-  };
-  console.log(user)
-  //  JSON string
-  const userJson = JSON.stringify(user);
-  
-  //  objecto en el local storage
-  localStorage.setItem(user.email, userJson);
-  
-  
+  if (passwordRegex.test(password)) {
+    // Password is valid
+    document.querySelector("#grupo__registro-password").classList.remove('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-password").classList.add('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-password i").classList.add('fa-check-circle');
+    document.querySelector("#grupo__registro-password i").classList.remove('fa-times-circle');
+    document.querySelector("#grupo__registro-password .formulario__input-error").classList.remove('formulario__input-error-activo');
+  } else {
+    // Password is invalid
+    document.querySelector("#grupo__registro-password").classList.add('formulario__grupo-incorrecto');
+    document.querySelector("#grupo__registro-password").classList.remove('formulario__grupo-correcto');
+    document.querySelector("#grupo__registro-password i").classList.add('fa-times-circle');
+    document.querySelector("#grupo__registro-password i").classList.remove('fa-check-circle');
+    document.querySelector("#grupo__registro-password .formulario__input-error").classList.add('formulario__input-error-activo');
   }
-  
-  export function validateLogin () {
-      // Traer el valor "user" key localStorage
-  
-  let userEmail = document.getElementById("logemail").value;
-  let userPassword = document.getElementById("logpass").value;
-  let userString = localStorage.getItem("userEmail");
-  
+  return (nameRegex.test(name) && emailRegex.test(email) && passwordRegex.test(password));
+
+}
+
+
+
+export function validateLogin() {
+  // Traer el valor "user" key localStorage
+
+  let userEmail = document.querySelector("#correo").value;
+  let userPassword = document.querySelector("#password").value;
+  let userString = localStorage.getItem(userEmail);
+
   // Checar si el usuario es valido
   if (userString === null) {
     // Usar una alerta si no coincide
-    alert('usuario no valido');
+    alert('usuario no es valido');
   } else {
     // User exists - do something with the user data
     var user = JSON.parse(userString);
     if (user.password === userPassword) {
       // Contraseña validada - hacer algo
-      alert('Contaseña valida,Bienvenido');
+      alert('Contaseña valida, Bienvenido');
     } else {
       // Si la contraseña es invalida - hacer otra cosa
       alert('La contraseña es invalida');
     }
   }
-  }
-  
+}
