4682f107f0057f47e38e585b08176141f6b0c4c4
username <manuelsk150@gmail.com>
2023-03-07 12:45:38 -0600

    Actualizacion GraciasCompra

--- src/components/GraciasCompra/GraciasCompra.jsx
+++ src/components/GraciasCompra/GraciasCompra.jsx
@@ -6,10 +6,19 @@ function GraciasCompra() {
   return (
     <div>  {/*Este es el codigo donde esta mis imagenes y los textos de la pagina Gracias por tu compra */}
       <div className="banner">
-        <img id="lucasanto" src="https://res.cloudinary.com/dbyl6ppep/image/upload/v1677978360/fotos/Luca_Santo_banner-02_1_fexvwl.png" alt="img lucasanto" />
+        <div className='LUCASANTOCENT'>
+           <img id="lucasanto" src="https://res.cloudinary.com/dbyl6ppep/image/upload/v1677978360/fotos/Luca_Santo_banner-02_1_fexvwl.png" alt="img lucasanto" />
+        </div>
+       
         <h1 className="thx">¡GRACIAS POR TU COMPRA!</h1>
+        <div className='contene-boton'>
+        
+        <button className="Back">Inicio</button>  
+       </div>
         <p className="breve">En breve te contactaremos <br /> via correo electronico <br /> con los detalles <br /> de tu pedido.</p>
       </div>
+      
+      
 
       <div className="h2yimg">
         <h2 className='achedos'>Bienvenúe a la familia</h2>
